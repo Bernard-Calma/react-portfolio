c9826fd0f4897d84df11725d2557ba3bc5bb2311
Bernard-Calma <nard.calma@gmail.com>
2023-01-05 00:29:23 -0800

    font style added

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -19,16 +19,16 @@ const Experience = () => {
                             <h4>Technologies</h4>
                             <div className="technologies">
                                 <div className="technologyContainer">
-                                    <img className = "logo" src="./public/java.png" alt="java" /> 
-                                    <p>Java</p>
+                                    <img className = "logo" src="images/education/java.png" alt="java" /> 
+                                    <p className="technologyTitles">Java</p>
                                 </div>
                                 <div className="technologyContainer">
-                                    <img className = "logo smallLogo" src="./public/eclipse.svg" alt="eclipse" />
-                                    <p>Eclipse IDE</p>
+                                    <img className = "logo smallLogo" src="images/education/eclipse.svg" alt="eclipse" />
+                                    <p className="technologyTitles">Eclipse IDE</p>
                                 </div>
                                 <div className="technologyContainer">
-                                    <img className = "logo smallLogo" src="./public/logo-data-structure.png" alt="data structure and algorithm" />
-                                    <p>Data Structure and Algorithm</p>
+                                    <img className = "logo smallLogo" src="images/education/logo-data-structure.png" alt="data structure and algorithm" />
+                                    <p className="technologyTitles">Data Structure and Algorithm</p>
                                 </div>
                             </div>
                         </div>
