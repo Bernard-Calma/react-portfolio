7ecd456ee28c08809e53b2cd959093024a683720
Bernard-Calma <nard.calma@gmail.com>
2023-01-05 00:49:31 -0800

    introduction added

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,6 +7,7 @@ const About = () => {
         <section className="aboutMe" id="aboutMe">
             <h1>Hi! I'm Bernard Calma</h1>
             <h3>I'm a Software Engineer</h3>
+            <p className="introduction">Highly motivated technology professional with more than 10 years of experience in tech and more than 1 year of experience with Full-Stack Development. Experience completing hardware and software troubleshooting, analysis and resolution procedures. Skilled in Six Sigma, Cloud Computing, Automation and Independent/Team Project Management.</p>
         </section>
     )
 }
