67742bc5d2d425f4c40a7b763f9b60a62bf4cfcd
Bernard-Calma <nard.calma@gmail.com>
2023-01-05 00:21:16 -0800

    experience section fonts styled

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,32 +2,32 @@ import "./Experience.css"
 
 const Experience = () => {
     return(
-        <section class = "experience title">
+        <section className = "experience title">
         <h2>Experience</h2>
         <h3>Education </h3> 
-        <div class="education">
+        <div className="education">
 
-            <div class = "asu school">
-                <div class="schoolTitle">
-                    <a href="https://engineering.asu.edu/" target="_blank"><img class = "schoolLogo" src="images/education/ASU.png" alt="asuLogo"/></a>
-                    <div class="schoolDescription">
-                        <h3 class="schoolName"> Arizona State University</h3>
+            <div className = "asu school">
+                <div className="schoolTitle">
+                    <a href="https://engineering.asu.edu/" target="_blank"><img className = "schoolLogo" src="images/education/ASU.png" alt="asuLogo"/></a>
+                    <div className="schoolDescription">
+                        <h3 className="schoolName"> Arizona State University</h3>
                         <h4>Software Engineering (BS)</h4>
-                        <p class = "yearGraduated">(Expected Graduation: 2026)</p>
-                        <p class = "courseDescription">- Software systems are complex, often including in excess of a million lines of code. Graduates of the BS in software engineering possess knowledge and skills of a defined engineering approach to complex systems analysis, planning, design and construction. The program has a unique, project-driven curriculum, establishing a new model of communication, teamwork, critical thinking and professionalism.</p>
-                        <div class = 'educationProjects'>
+                        <p className = "yearGraduated">(Expected Graduation: 2026)</p>
+                        <p className = "courseDescription">- Software systems are complex, often including in excess of a million lines of code. Graduates of the BS in software engineering possess knowledge and skills of a defined engineering approach to complex systems analysis, planning, design and construction. The program has a unique, project-driven curriculum, establishing a new model of communication, teamwork, critical thinking and professionalism.</p>
+                        <div className = 'educationProjects'>
                             <h4>Technologies</h4>
-                            <div class="technologies">
-                                <div class="technologyContainer">
-                                    <img class = "logo" src="./public/java.png" alt="java" /> 
+                            <div className="technologies">
+                                <div className="technologyContainer">
+                                    <img className = "logo" src="./public/java.png" alt="java" /> 
                                     <p>Java</p>
                                 </div>
-                                <div class="technologyContainer">
-                                    <img class = "logo smallLogo" src="./public/eclipse.svg" alt="eclipse" />
+                                <div className="technologyContainer">
+                                    <img className = "logo smallLogo" src="./public/eclipse.svg" alt="eclipse" />
                                     <p>Eclipse IDE</p>
                                 </div>
-                                <div class="technologyContainer">
-                                    <img class = "logo smallLogo" src="./public/logo-data-structure.png" alt="data structure and algorithm" />
+                                <div className="technologyContainer">
+                                    <img className = "logo smallLogo" src="./public/logo-data-structure.png" alt="data structure and algorithm" />
                                     <p>Data Structure and Algorithm</p>
                                 </div>
                             </div>
@@ -38,17 +38,17 @@ const Experience = () => {
             </div>
 
             <hr/>
-            <div class = "generalAssembly school">
-                <div class="schoolTitle">
-                    <a href="https://generalassemb.ly/" target="_blank"><img class = "gaSchoolLogo" src="images/education/GA.png" alt="gaLogo"/></a>
-                    <div class="gaSchoolDescription">
-                        <h3 class="schoolName"> General Assembly</h3>
+            <div className = "generalAssembly school">
+                <div className="schoolTitle">
+                    <a href="https://generalassemb.ly/" target="_blank"><img className = "gaSchoolLogo" src="images/education/GA.png" alt="gaLogo"/></a>
+                    <div className="gaSchoolDescription">
+                        <h3 className="schoolName"> General Assembly</h3>
                         <h4>Software Engineering Immersive</h4>
-                        <p class = "yearGraduated">(Graduated: 2022)</p>
-                        <p class = "courseDescription">- Software engineering immersive student in an intensive, twelve-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developing a portfolio of individual and group projects.</p>
-                        <div class = 'educationProjects'>
+                        <p className = "yearGraduated">(Graduated: 2022)</p>
+                        <p className = "courseDescription">- Software engineering immersive student in an intensive, twelve-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developing a portfolio of individual and group projects.</p>
+                        <div className = 'educationProjects'>
                             <h4>Projects</h4>
-                            <ul class="educationProjects">
+                            <ul className="educationProjects">
                                 <li>Streamin (Social Media Platform like)- React, Mongodb, Express, NodeJS, HTML, CSS, Javascript</li>
                                 <li>Spentrace (Butgeting App) - EJS, MongoDB, Express, NodeJS, HTML, CSS, Javascript</li>
                                 <li>Floating Shoe Game - JavaScript, HTML Canvas, CSS</li>
@@ -59,17 +59,17 @@ const Experience = () => {
                 </div>
             </div>
             <hr/>
-            <div class = "sti school">
-                <div class="schoolTitle">
-                    <a href="https://www.sti.edu/" target="_blank"><img class = "stiSchoolLogo" src="images/education/STI.png" alt="stiLogo"/></a>
-                    <div class="schoolDescription">
-                        <h3 class="schoolName">STI College Bacoor</h3>
+            <div className = "sti school">
+                <div className="schoolTitle">
+                    <a href="https://www.sti.edu/" target="_blank"><img className = "stiSchoolLogo" src="images/education/STI.png" alt="stiLogo"/></a>
+                    <div className="schoolDescription">
+                        <h3 className="schoolName">STI College Bacoor</h3>
                         <h4>Computer Technology (AS)</h4>
-                        <p class = "yearGraduated">(Graduated: 2009)</p>
-                        <p class = "courseDescription">- Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.</p>
-                        <div class = 'educationProjects'>
+                        <p className = "yearGraduated">(Graduated: 2009)</p>
+                        <p className = "courseDescription">- Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.</p>
+                        <div className = 'educationProjects'>
                             <h4>Projects</h4>
-                            <ul class="educationProjects">
+                            <ul className="educationProjects">
                                 <li>Social Media website - Java, NetBeans</li>
                                 <li>Hotel Room and Reservation System - VB.net, Microsoft Access</li>
                                 <li>Powering a Radio using a Solar Panel - Arduino</li>
