8f8403790c2a7e9e48964e2028ccf5e45b90e363
Bernard-Calma <nard.calma@gmail.com>
2023-01-05 00:46:25 -0800

    added styles on about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
+// IMPORTS
+    // Styles
+    import "./About.css"
+
 const About = () => {
     return(
-        <>
+        <section className="aboutMe" id="aboutMe">
             <h1>Hi! I'm Bernard Calma</h1>
             <h3>I'm a Software Engineer</h3>
-        </>
+        </section>
     )
 }
 
