e2b1653c6f0a8875abb8a3a09f9f44cfbe06c754
Bernard-Calma <nard.calma@gmail.com>
2023-01-06 18:29:09 -0800

    added Porject folder where all projects will be saved

---
+++ src/projects/WebsiteProjects.js
@@ -0,0 +1,10 @@
+const WebsiteProjects = [{
+    name: "The Tamagotchi Project",
+    websiteLink: "https://github.com/Bernard-Calma/tamagotchi-project",
+    summary: "First mini project at General Assembly based on a 90s game called Tamagotchi.",
+    skills: ["HTML", "CSS", "JavaScript"]
+}]
+
+console.log(WebsiteProjects)
+
+export default WebsiteProjects
