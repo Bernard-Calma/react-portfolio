b0d1b1303de4664943a0ff8c45b7687f86c83676
nard-calma <nard.calma@gmail.com>
2023-01-08 16:18:58 -0800

    asu and sti schools styled completely

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,15 +8,14 @@ const Experience = () => {
         <div className="education">
 
             <div className = "asu school">
-                <div className="schoolTitle">
                     <a href="https://engineering.asu.edu/" target="_blank"><img className = "schoolLogo" src="images/education/ASU.png" alt="asuLogo"/></a>
                     <div className="schoolDescription">
                         <h3 className="schoolName"> Arizona State University</h3>
-                        <h4>Software Engineering (BS)</h4>
+                        <h4 className="course">Software Engineering (BS)</h4>
                         <p className = "yearGraduated">(Expected Graduation: 2026)</p>
                         <p className = "courseDescription">- Software systems are complex, often including in excess of a million lines of code. Graduates of the BS in software engineering possess knowledge and skills of a defined engineering approach to complex systems analysis, planning, design and construction. The program has a unique, project-driven curriculum, establishing a new model of communication, teamwork, critical thinking and professionalism.</p>
                         <div className = 'educationProjects'>
-                            <h4>Technologies</h4>
+                            <h4 className="technologiesTitle">Technologies</h4>
                             <div className="technologies">
                                 <div className="technologyContainer">
                                     <img className = "logo" src="images/education/java.png" alt="java" /> 
@@ -27,14 +26,12 @@ const Experience = () => {
                                     <p className="technologyTitles">Eclipse IDE</p>
                                 </div>
                                 <div className="technologyContainer">
-                                    <img className = "logo smallLogo" src="images/education/logo-data-structure.png" alt="data structure and algorithm" />
+                                    <img className = "logo smallLogo dataStructureLogo" src="images/education/logo-data-structure.png" alt="data structure and algorithm" />
                                     <p className="technologyTitles">Data Structure and Algorithm</p>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    
-                </div>
             </div>
             <hr/>
 
@@ -43,11 +40,11 @@ const Experience = () => {
                     <a href="https://generalassemb.ly/" target="_blank"><img className = "gaSchoolLogo" src="images/education/GA.png" alt="gaLogo"/></a>
                     <div className="gaSchoolDescription">
                         <h3 className="schoolName"> General Assembly</h3>
-                        <h4>Software Engineering Immersive</h4>
+                        <h4 className="course">Software Engineering Immersive</h4>
                         <p className = "yearGraduated">(Graduated: 2022)</p>
                         <p className = "courseDescription">- Software engineering immersive student in an intensive, twelve-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developing a portfolio of individual and group projects.</p>
                         <div className = 'educationProjects'>
-                            <h4>Projects</h4>
+                            <h4 className="course">Projects</h4>
                             <ul className="educationProjects">
                                 <li>Streamin (Social Media Platform like)- React, Mongodb, Express, NodeJS, HTML, CSS, Javascript</li>
                                 <li>Spentrace (Butgeting App) - EJS, MongoDB, Express, NodeJS, HTML, CSS, Javascript</li>
@@ -61,15 +58,14 @@ const Experience = () => {
             <hr/>
 
             <div className = "sti school">
-                <div className="gaSchoolTitle">
                     <a href="https://www.sti.edu/" target="_blank"><img className = "schoolLogo" src="images/education/STI.png" alt="stiLogo"/></a>
                     <div className="schoolDescription">
                         <h3 className="schoolName">STI College Bacoor</h3>
-                        <h4>Computer Technology (AS)</h4>
+                        <h4 className="course">Computer Technology (AS)</h4>
                         <p className = "yearGraduated">(Graduated: 2009)</p>
                         <p className = "courseDescription">- Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.</p>
                         <div className = 'educationProjects'>
-                            <h4>Projects</h4>
+                            <h4 className="course">Projects</h4>
                             <ul className="educationProjects">
                                 <li>Social Media website - Java, NetBeans</li>
                                 <li>Hotel Room and Reservation System - VB.net, Microsoft Access</li>
@@ -77,8 +73,6 @@ const Experience = () => {
                             </ul>
                         </div>
                     </div>
-                    
-                </div>
             </div> 
             <hr />
         </div>
