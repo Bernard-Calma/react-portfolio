632c6ed7c6b383da991566785c68d1b042e49b30
nard-calma <nard.calma@gmail.com>
2023-01-13 18:21:02 -0800

    footer added and styled

---
+++ src/containers/Footer.jsx
@@ -0,0 +1,6 @@
+const Footer = () => 
+    <div>
+        <p class = "footer">ⓒ Bernard Calma 2023</p>
+    </div>
+
+export default Footer
--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -3,6 +3,7 @@ import Contacts from "../components/Contacts"
 import Experience from "../components/Experience"
 import Headers from "../components/Header"
 import Projects from "../components/Projects"
+import Footer from "./Footer"
 
 const Main = () => {
     return (
@@ -12,6 +13,7 @@ const Main = () => {
             <Experience />
             <Projects />
             <Contacts />
+            <Footer />
         </main>
     )
 }
