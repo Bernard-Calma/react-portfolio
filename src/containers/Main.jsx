bfd4ab45bb9a0e7ab87409b1de17ea9755e3d5b2
nard-calma <nard.calma@gmail.com>
2023-01-14 18:44:21 -0800

    remved headers in main, not being used

--- src/containers/Main.jsx
+++ src/containers/Main.jsx
@@ -1,14 +1,12 @@
 import About from "../components/About"
 import Contacts from "../components/Contacts"
 import Experience from "../components/Experience"
-import Headers from "../components/Header"
 import Projects from "../components/Projects"
 import Footer from "./Footer"
 
 const Main = () => {
     return (
         <main>
-            <Headers />
             <About />
             <Experience />
             <Projects />
