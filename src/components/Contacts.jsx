9b93ec824e355ebb893a7a0ee1d23847dcea5b88
nard-calma <nard.calma@gmail.com>
2023-01-16 17:27:19 -0800

    added  rel=noreferrer n all a tag in contact.jsx to fix warning

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -7,9 +7,9 @@ const Contacts = () => {
             <div>
                 <h3 className="sectionSubTitle">Social Media</h3>
                 <ul class="socialMedia">
-                    <li class = "socialMediaItem"><a href="https://www.linkedin.com/in/bernard-calma/" target="_blank"><i class="fa-brands fa-linkedin"></i></a></li>
-                    <li class = "socialMediaItem"><a href="https://github.com/Bernard-Calma" target="_blank"><i class="fa-brands fa-github"></i></a></li>
-                    <li class = "socialMediaItem"><a href="https://twitter.com/ryznrd" target="_blank"><i class="fa-brands fa-twitter"></i></a></li>
+                    <li class = "socialMediaItem"><a href="https://www.linkedin.com/in/bernard-calma/" target="_blank"  rel="noreferrer"><i class="fa-brands fa-linkedin"></i></a></li>
+                    <li class = "socialMediaItem"><a href="https://github.com/Bernard-Calma" target="_blank"  rel="noreferrer"><i class="fa-brands fa-github"></i></a></li>
+                    <li class = "socialMediaItem"><a href="https://twitter.com/ryznrd" target="_blank"  rel="noreferrer"><i class="fa-brands fa-twitter"></i></a></li>
                 </ul>
                 <ul class="contactInformation">
                     <li>Location: Lancaster, CA</li>
