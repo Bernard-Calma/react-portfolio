e1c2ae3dde4c935a462b18b3e7202055ed96cd3d
nard-calma <nard.calma@gmail.com>
2023-01-17 18:45:52 -0800

    techonologies map added in technologies jsx to show different techs

--- src/containers/Technologies.jsx
+++ src/containers/Technologies.jsx
@@ -1,21 +1,16 @@
-const Technologies = () => {
+import { useState } from "react"
+
+const Technologies = (props) => {
+    const [technologies, setTechnologies] = useState(props.technologies)
     return(
         <div>
             <h4 className="technologiesTitle">Technologies</h4>
-            <div className="technologies">
+            {technologies.map(technology => 
                 <div className="technologyContainer">
-                    <img className = "logo" src="images/education/java.png" alt="java" /> 
-                    <p className="technologyTitles">Java</p>
+                    <img className = "logo" src={technology.logo} alt={technology.title} /> 
+                    <p className="technologyTitles">{technology.title}</p>
                 </div>
-                <div className="technologyContainer">
-                    <img className = "logo smallLogo" src="images/education/eclipse.svg" alt="eclipse" />
-                    <p className="technologyTitles">Eclipse IDE</p>
-                </div>
-                <div className="technologyContainer">
-                    <img className = "logo smallLogo dataStructureLogo" src="images/education/logo-data-structure.png" alt="data structure and algorithm" />
-                    <p className="technologyTitles">Data Structure and Algorithm</p>
-                </div>
-            </div>
+            )}
         </div>
     )
 }
