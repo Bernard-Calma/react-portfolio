fa101f45597db36f918aa199484b4dbd19835d1e
nard-calma <nard.calma@gmail.com>
2023-01-17 18:24:02 -0800

    moved asu div inside school component and call school component in education

--- src/components/School.jsx
+++ src/components/School.jsx
@@ -1,8 +1,31 @@
 const School = (props) => {
     return(
-        <div>
-
+        <div className = "asu school">
+        <a href="https://engineering.asu.edu/" target="_blank"  rel="noreferrer"><img className = "asuSchoolLogo" src="images/education/ASU.png" alt="asuLogo"/></a>
+        <div className="schoolDescription">
+            <h3 className="schoolName"> Arizona State University</h3>
+            <h4 className="course">Software Engineering (BS)</h4>
+            <p className = "yearGraduated">(Expected Graduation: 2026)</p>
+            <p className = "courseDescription">- Software systems are complex, often including in excess of a million lines of code. Graduates of the BS in software engineering possess knowledge and skills of a defined engineering approach to complex systems analysis, planning, design and construction. The program has a unique, project-driven curriculum, establishing a new model of communication, teamwork, critical thinking and professionalism.</p>
+            <div className = 'educationProjects'>
+                <h4 className="technologiesTitle">Technologies</h4>
+                <div className="technologies">
+                    <div className="technologyContainer">
+                        <img className = "logo" src="images/education/java.png" alt="java" /> 
+                        <p className="technologyTitles">Java</p>
+                    </div>
+                    <div className="technologyContainer">
+                        <img className = "logo smallLogo" src="images/education/eclipse.svg" alt="eclipse" />
+                        <p className="technologyTitles">Eclipse IDE</p>
+                    </div>
+                    <div className="technologyContainer">
+                        <img className = "logo smallLogo dataStructureLogo" src="images/education/logo-data-structure.png" alt="data structure and algorithm" />
+                        <p className="technologyTitles">Data Structure and Algorithm</p>
+                    </div>
+                </div>
+            </div>
         </div>
+    </div>      
     )
 }
 
--- src/containers/Education.jsx
+++ src/containers/Education.jsx
@@ -1,36 +1,12 @@
 import { useState } from "react"
+import School from "../components/School"
 import Schools from "../modules/Schools"
 
 const Education = () => {
     const [schools, setSchools] = useState(Schools)
     return(
         <div className="education">
-            <div className = "asu school">
-                    <a href="https://engineering.asu.edu/" target="_blank"  rel="noreferrer"><img className = "asuSchoolLogo" src="images/education/ASU.png" alt="asuLogo"/></a>
-                    <div className="schoolDescription">
-                        <h3 className="schoolName"> Arizona State University</h3>
-                        <h4 className="course">Software Engineering (BS)</h4>
-                        <p className = "yearGraduated">(Expected Graduation: 2026)</p>
-                        <p className = "courseDescription">- Software systems are complex, often including in excess of a million lines of code. Graduates of the BS in software engineering possess knowledge and skills of a defined engineering approach to complex systems analysis, planning, design and construction. The program has a unique, project-driven curriculum, establishing a new model of communication, teamwork, critical thinking and professionalism.</p>
-                        <div className = 'educationProjects'>
-                            <h4 className="technologiesTitle">Technologies</h4>
-                            <div className="technologies">
-                                <div className="technologyContainer">
-                                    <img className = "logo" src="images/education/java.png" alt="java" /> 
-                                    <p className="technologyTitles">Java</p>
-                                </div>
-                                <div className="technologyContainer">
-                                    <img className = "logo smallLogo" src="images/education/eclipse.svg" alt="eclipse" />
-                                    <p className="technologyTitles">Eclipse IDE</p>
-                                </div>
-                                <div className="technologyContainer">
-                                    <img className = "logo smallLogo dataStructureLogo" src="images/education/logo-data-structure.png" alt="data structure and algorithm" />
-                                    <p className="technologyTitles">Data Structure and Algorithm</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            </div>
+            <School />
             <hr/>
             <div className = "generalAssembly school">
                 <div className="schoolTitle">
