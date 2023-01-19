2dd8a24aaea629d595ee753d166c1e11e2e1540f
nard-calma <nard.calma@gmail.com>
2023-01-18 23:35:38 -0800

    project tag order changed

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,13 +4,13 @@ const Project = (props) => {
     const [project, setProject] = useState(props.project)
     return(
         <div className={props.index === 0? "carousel-item active relative float-left w-full" : "carousel-item relative float-left w-full"}>
+        <a class="websiteLink" href={project.websiteLink} target="_blank"><h5 className="text-xl projectName">{project.name}</h5></a>
         <img
             src={project.screenshot}
             className="projectImage block w-full"
             alt="Streamin Screenshot"
         />
-        <div className=" projectSummary carousel-caption hidden md:block absolute text-center">
-        <a class="websiteLink" href={project.websiteLink} target="_blank"><h5 className="text-xl projectName">{project.name}</h5></a>
+        <div className=" projectSummary carousel-caption md:block absolute text-center">
             <div className="projectDescription">
             <p>{project.summary}</p>
             Skills:
