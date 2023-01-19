93641ded9c1453cf7b682e517bfc36c3617d20d7
nard-calma <nard.calma@gmail.com>
2023-01-18 22:20:33 -0800

    removed STI in experience and added in School Modules to show up in School component

--- src/containers/Education.jsx
+++ src/containers/Education.jsx
@@ -13,23 +13,6 @@ const Education = () => {
                     key = {index}   
                 />
             )}
-            <div className = "sti school">
-                    <a href="https://www.sti.edu/" target="_blank"  rel="noreferrer"><img className = "schoolLogo" src="images/education/STI.png" alt="stiLogo"/></a>
-                    <div className="schoolDescription">
-                        <h3 className="schoolName">STI College Bacoor</h3>
-                        <h4 className="course">Computer Technology (AS)</h4>
-                        <p className = "yearGraduated">(Graduated: 2009)</p>
-                        <p className = "courseDescription">- Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.</p>
-                        <div className = 'educationProjects'>
-                            <h4 className="course">Projects</h4>
-                            <ul className="educationProjects">
-                                <li className="educationProjects">Social Media website - Java, NetBeans</li>
-                                <li className="educationProjects">Hotel Room and Reservation System - VB.net, Microsoft Access</li>
-                                <li className="educationProjects">Powering a Radio using a Solar Panel - Arduino</li>
-                            </ul>
-                        </div>
-                    </div>
-            </div> 
         </div>
     )
 }
--- src/modules/Schools.js
+++ src/modules/Schools.js
@@ -25,7 +25,7 @@ const Schools = [{
     course: "Software Engineering",
     degree: "Certificate",
     yearGraduate: "(Graduated: 2022)",
-    schoolWebsite: "https://engineering.asu.edu/",
+    schoolWebsite: "https://generalassemb.ly/",
     description: "- Software engineering immersive student in an intensive, twelve-week, 450+ hour program focused on product development fundamentals, object-oriented programming, MVC frameworks, data modeling, and team collaboration strategies. Developing a portfolio of individual and group projects.",
     image: "images/education/GA.png",    
     projects: [
@@ -33,6 +33,20 @@ const Schools = [{
         "Spentrace (Butgeting App) - EJS, MongoDB, Express, NodeJS, HTML, CSS, Javascript",
         "Floating Shoe Game - JavaScript, HTML Canvas, CSS",
         "Tamagotchi Game - JavaScript, HTML, CSS"
+    ]
+},
+{
+    name: "STI College Bacoor",
+    course: "Computer Technology",
+    degree: "AS",
+    yearGraduate: "(Graduated: 2009)",
+    schoolWebsite: "https://www.sti.edu/",
+    description: "- Studied various programming methodologies, computer networks and concepts, basic computation, and data processing.",
+    image: "images/education/STI.png",    
+    projects: [
+        "Social Media website - Java, NetBeans",
+        "Hotel Room and Reservation System - VB.net, Microsoft Access",
+        "Powering a Radio using a Solar Panel - Arduino",
     ]
 }]
 
