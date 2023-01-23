f169cee8e7cc7b74b78e8404e82e2c0fe35905d8
Bernard-Calma <nard.calma@gmail.com>
2023-01-22 19:45:30 -0800

    added Works module and addeed Sutherland job

---
+++ src/modules/Works.js
@@ -0,0 +1,16 @@
+const Works = [
+    {
+        company: "Sutherland Global Services",
+        position: "Subject Matter Expert",
+        startMonth: 3,
+        startYear: 2014,
+        endMonth: 1,
+        endYear: 2018,
+        jobDescription: `• Provided support and management to different teams and projects to improve service level for different accounts.
+        • Supervised meetings/huddles with different teams to provide action plans and coachings to improve the team's stats.
+        • Collaborated with upper management and other departments to analyze data and process quality to make sure it meets the client’s requirements and exceeds expectations.
+        • Handled customer’s issues and concerns regarding their Internet, Analog/Digital Phone and TV and provided troubleshooting steps to resolve any issue.`,
+    }
+]
+
+module.exports = Works
